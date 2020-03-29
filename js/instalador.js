59363b0a0f48d8b6ef2eac7eac3261d4fc85fb85
Odair Cardoso Martins <omartins@fitesa.com>
2020-03-29 18:24:03 -0300

    terceiro commit

--- js/instalador.js
+++ js/instalador.js
@@ -1,10 +1,10 @@
 let newWorker;
 
 if ("serviceWorker" in navigator) {
-navigator.serviceWorker
+    navigator.serviceWorker
     .register("./chopeiraSW.js") // [A]
     .then(function(registration) {
-    registration.addEventListener("updatefound", () => { // [B]
+        registration.addEventListener("updatefound", () => { // [B]
         // Uma atualização no Service Worker foi encontrada, instalando...
         console.log("1 - Uma atualização no Service Worker foi encontrada, instalando...");
         newWorker = registration.installing; // [C]
@@ -13,23 +13,23 @@ navigator.serviceWorker
             // O estado do Service Worker mudou?
             console.log("2 - O estado do Service Worker mudou?");
             switch (newWorker.state) {
-                case "installed": {
-                    // Existe um novo Service Worker disponível, mostra a notificação
-                    console.log("3 - Existe um novo Service Worker disponível, mostra a notificação");
-                    if (navigator.serviceWorker.controller) {
-                        document.getElementById('update-button').style.display = "block";
-                        // O evento de clique na notificação
-                        document.getElementById("update-button").addEventListener("click", function() {
-                            newWorker.postMessage({ action: "skipWaiting" });
-                        })  
-                        break;
-                    }
+            case "installed": {
+                // Existe um novo Service Worker disponível, mostra a notificação
+                console.log("3 - Existe um novo Service Worker disponível, mostra a notificação");
+                if (navigator.serviceWorker.controller) {
+                document.getElementById('update-button').style.display = "block";
+                // O evento de clique na notificação
+                document.getElementById("update-button").addEventListener("click", function() {
+                    newWorker.postMessage({ action: "skipWaiting" });
+                })  
+                break;
                 }
             }
+            }
         });
-    });
+        });
 
-    // SUCESSO - ServiceWorker Registrado
+        // SUCESSO - ServiceWorker Registrado
         console.log("4 - ServiceWorker registrado com sucesso no escopo: ", registration.scope);
     })
     .catch(function(err) {
@@ -55,18 +55,18 @@ window.addEventListener('beforeinstallprompt', (e) => {
     document.getElementById('add-button').style.display = "block";
 
     document.getElementById('add-button').addEventListener('click', (e) => {
-        // hide our user interface that shows our A2HS button
-        document.getElementById('add-button').style.display = 'none';
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
+    // hide our user interface that shows our A2HS button
+    document.getElementById('add-button').style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
             console.log('User accepted the A2HS prompt');
-            } else {
+        } else {
             console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
+        }
+        deferredPrompt = null;
         });
     });
 });
@@ -75,7 +75,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
 // Aqui estamos recarregando a página
 navigator.serviceWorker.addEventListener("controllerchange", function() {
     if (refreshing) {
-        return;
+    return;
     }
     window.location.reload();
     refreshing = true;
